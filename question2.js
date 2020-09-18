cc80bbe25da9789d4542c28b365f6ba2b43f5112
Ankita Pandey <69104540+AnkitaPandey99@users.noreply.github.com>
2020-09-18 06:51:40 -0700

    Update question2.js

--- question2.js
+++ question2.js
@@ -1,4 +1,5 @@
-let bus=[
+window.onload = function(){
+    let initialbus=[
     {
         name:"bus1",
         source:"mumbai",
@@ -20,16 +21,25 @@ let bus=[
         destination:"ladakh",
         number: 1002,
         capacity: 50
-    }
-    
+    } 
     ];
+    
+    if (localStorage.getItem("bus") == null) {
+        localStorage.setItem("bus", JSON.stringify(initialbus));
+      }
+};
 
-
-    function display(superbus){
-        let srno=1;
-        let tabledata=""   //tabledata is blank cpz everytime display is called it creates table from d beginning
+    function display(freshtable = undefined){
+        let tabledata="";   //tabledata is blank cpz everytime display is called it creates table from d beginning
+        let bus;
+        if (freshtable == undefined) {
+            bus = JSON.parse(localStorage.getItem("bus"));
+          } else {
+            bus = freshtable;
+          }
         // for loop
-        superbus.forEach(function(entry,index){
+        let srno=1;
+        bus.forEach(function(entry, index){
         
         let currentrow=`<tr>
         <td>${srno}</td>
@@ -42,11 +52,12 @@ let bus=[
         srno++;
        // for the next loop iteration
        tabledata+=currentrow;
-       });
        
+       });
+        
         document.getElementsByClassName('tdata')[0].innerHTML=tabledata;
     }
-    display(bus);  // display the pre-existing array
+    display();  // display the pre-existing array
 
     //  function to add bus
     function addEntry(event){
@@ -59,42 +70,46 @@ let bus=[
         let number = document.getElementById("number").value;
         let capacity = document.getElementById("capacity").value;
         
-        employee.name = name;
-        employee.source = source;
-        employee.destination = destination;
-        employee.number = Number(number);
-        employee.capacity = Number(capacity);
+        entry.name = name;
+        entry.source = source;
+        entry.destination = destination;
+        entry.number = Number(number);
+        entry.capacity = Number(capacity);
+
+        // bus.push(entry)
+        
+
+        // local storage
+        let bus=JSON.parse(localStorage.getItem("bus"));
+        bus.push(entry);
+        localStorage.setItem("bus", JSON.stringify(bus));
+
+      display();
     
 
-        // lets add the new-data into existing array by using push()
-        bus.push(entry);
-        // console.log(employees); this will show newly added data with existing objects in console
+    
+         console.log(employees); //this will show newly added data with existing objects in console
 
-     display(bus);    //display fresh array
-
+        
      // after display make the value in the form blank   
         document.getElementById("name").value="";
         document.getElementById("source").value="";
         document.getElementById("destination").value="";
         document.getElementById("number").value="";
         document.getElementById("capacity").value="";
-    
     }
 
-
-
-
-
-     //create search function
-     function searchBySourceAndDestination() {
+    //create search function
+    function searchBySourceAndDestination() {
     
         let searchValue=document.getElementById("search").value;
-        // console.log(searchValue);
+        console.log(searchValue);
         // return the index of name if it not -1 that implies our searched input is atleast anywhere in the table
         //EXAMPLE: var ele="hello" <- ele.indexOf("e"); <-1 <- ele.indexOf("s");<- -1
-       let newdata=employees.filter(function(employee){
-            return (employee.name.toUpperCase().indexOf(searchValue.toUpperCase()) != -1) ||
-                   (employee.city.toUpperCase().indexOf(searchValue.toUpperCase()) != -1);
+       let bus = JSON.parse(localStorage.getItem("bus"));
+       let newdata=bus.filter(function(entry){
+            return (entry.source.toUpperCase().indexOf(searchValue.toUpperCase()) != -1) ||
+                   (entry.destination.toUpperCase().indexOf(searchValue.toUpperCase()) != -1);
         });
 
         // console.log(newdata);
@@ -102,4 +117,4 @@ let bus=[
        
         display(newdata);    // display newdata 
     }
-   
+
